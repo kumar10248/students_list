53a3bfeda2cba3e0230b32de03f16e0eb8a5abb5
kumar Devashish <kumardevashish000@gmail.com>
2024-05-23 01:48:55 +0530

    first commit

---
+++ src/components/Home.tsx
@@ -0,0 +1,57 @@
+import  { useEffect, useState } from 'react';
+import './Home.css';
+
+import defaultProfilePicture from 'C:/Users/kumar/Contacts/Desktop/defaultProfilePicture.png';
+import axios from 'axios';
+
+const Home = () => {
+ const [searchText,setSearchText]=useState<string>('');
+ const [data,setData]=useState<any>([])
+useEffect(()=>{
+  const fetchData= async()=>{
+    const res = await axios.get('http://localhost:3000/all');
+  setData(res.data.student);
+  }
+fetchData();
+},[]);   
+
+
+const searchData = data.filter((student:any)=>
+student.name.toLowerCase().includes(searchText.toLowerCase())
+|| student.section.toLowerCase().includes(searchText.toLowerCase())
+||student.uid.toLowerCase().includes(searchText.toLowerCase())
+)
+
+  return (
+    <>
+    <div className='sr'>
+      <input className='search' value={searchText} onChange={(e)=>{setSearchText(e.target.value)}} type="text" />
+    </div>
+    <div className='main'>
+    
+      {searchData.map((student:any, index:any) => (
+        
+        <div
+          className='stcard'
+          key={index}
+          
+        >
+          <img
+            src={defaultProfilePicture}
+            alt={`${student.name}'s profile`}
+            className='profile-picture'
+          />
+          <div className='name'>{student.name}</div>
+          <div>UID: {student.uid}</div>
+          <div>Section: {student.section}</div>
+          <div>Group: {student.group}</div>
+          <div>Batch: {student.batch}</div>
+          
+        </div>
+      ))}
+    </div>
+    </>
+  );
+};
+
+export default Home;
