783019437cbcf06654a0733f8ef947e66831537a
kumar Devashish <kumardevashish000@gmail.com>
2024-06-15 23:25:28 +0530

    chore: Update Home component to include lazy loading for images

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import './App.css'
-import Home from './components/Home'
+import "./App.css";
+import Home from "./components/Home";
 
 function App() {
-  
-
   return (
     <>
-  <Home/>
+      <Home />
     </>
-   
-  )
+  );
 }
 
-export default App
+export default App;
